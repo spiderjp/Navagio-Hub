6df445156efc07a02d0c1aeb75610ff27a177ae1
João Pedro <jotapejpbsf@gmail.com>
2023-09-01 15:19:15 -0300

    Fixing form submission and final destination

--- app.js
+++ app.js
@@ -1,119 +1,75 @@
-require('dotenv').config();
+const express = require('express');
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-connection.connect((error) => {
-    if (error) {
-        console.error('Erro ao conectar ao banco de dados:', error);
-    } else {
-        console.log('Conectado ao PlanetScale!');
-    }
-});
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+  });
 
-//SENDING DATAS TO LEADS TABLE
-
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+  
 
 app.post('/submit-form', (req, res) => {
-    
-    const { name, phone, email } = req.body;
 
-    if (!name || !phone || !email) {
-        return res.status(400).json({ error: 'Todos os campos devem ser preenchidos.' });
+  const { name, phone, email } = req.body;
+
+  // Verifique se todos os campos estão preenchidos
+  if (!name || !phone || !email) {
+    return res.status(400).send('Todos os campos são obrigatórios.');
+  }
+
+  // Verifique se o telefone contém apenas números
+  if (!/^[0-9]+$/.test(phone)) {
+    return res.status(400).send('O telefone deve conter apenas números.');
+  }
+
+  // Verifique se o e-mail contém '@'
+  if (!email.includes('@')) {
+    return res.status(400).send('O e-mail deve conter um "@".');
+  }
+
+  // Verifique se já existe um registro com o mesmo telefone ou e-mail
+  connection.query(
+    'SELECT * FROM leads WHERE phone = ? OR email = ?',
+    [phone, email],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Erro no servidor.');
+      }
+
+      if (results.length > 0) {
+        return res.status(400).send('Este telefone ou e-mail já está cadastrado.');
+      }
+
+      // Se não houver duplicação, insira os dados na tabela
+      connection.query(
+        'INSERT INTO leads (name, phone, email) VALUES (?, ?, ?)',
+        [name, phone, email],
+        (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).send('Erro no servidor.');
+          }
+
+          // Redirecione para a página de confirmação após a inserção bem-sucedida
+          res.redirect('/html/confirmation_sendForm.html');
+        }
+      );
     }
-
-    if (!/^\d+$/.test(phone)) {
-        return res.status(400).json({ error: 'Campo telefone preenchido incorretamente, por favor arrume antes de enviar.' });
-    }
-
-    if (!email.includes('@')) {
-        return res.status(400).json({ error: 'Campo e-mail preenchido incorretamente, por favor arrume antes de enviar.' });
-    }
-
-    const insertLeadQuery = `
-        INSERT INTO leads (name, phone, email)
-        VALUES (?, ?, ?)
-    `;
-
-    connection.query(insertLeadQuery, [name, phone, email], (error, results) => {
-        if (error) {
-            console.error('Erro ao inserir dados:', error);
-            return res.status(500).json({ error: 'Erro ao processar a requisição.' });
-        } else {
-            return res.redirect(`/public/html/confirmation-sendForm.html?name=${name}`);
-        }
-    });
+  );
 });
-
-
-
-//  SEARCHING ALL REGISTERS IN LEADS TABLE
-
-app.get('/leads', (req, res) => {
-    const selectLeadsQuery = `
-        SELECT * FROM leads
-    `;
-
-    connection.query(selectLeadsQuery, (error, results) => {
-        if (error) {
-            console.error('Erro ao consultar dados:', error);
-            return res.status(500).json({ error: 'Erro ao processar a requisição.' });
-        } else {
-            return res.status(200).json(results);
-        }
-    });
-});
-
-
-
-//  SEARCHING ONE REGISTER IN LEADS TABLE
-
-app.get('/leads/search', (req, res) => {
-    const { name } = req.query;
-    const selectLeadsByNameQuery = `
-        SELECT * FROM leads WHERE name = ?
-    `;
-
-    connection.query(selectLeadsByNameQuery, [name], (error, results) => {
-        if (error) {
-            console.error('Erro ao consultar dados:', error);
-            return res.status(500).json({ error: 'Erro ao processar a requisição.' });
-        } else {
-            return res.status(200).json(results);
-        }
-    });
-});
-
-
-
-//  DELETING ONE REGISTER IN LEADS TABLE
-
-app.delete('/leads/:id', (req, res) => {
-    const { id } = req.params;
-    const deleteLeadQuery = `
-        DELETE FROM leads WHERE id = ?
-    `;
-
-    connection.query(deleteLeadQuery, [id], (error, results) => {
-        if (error) {
-            console.error('Erro ao excluir registro:', error);
-            return res.status(500).json({ error: 'Erro ao processar a requisição.' });
-        } else {
-            return res.status(200).json({ message: 'Registro excluído com sucesso.' });
-        }
-    });
-});
-
 
 app.listen(3000, () => {
-    console.log('Servidor iniciado na porta 3000.');
+  console.log('Server is running on port 3000');
 });
-
-
